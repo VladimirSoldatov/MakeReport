3d35234b12cc020e28938a3f25d0669492a7e1ef
Владимир Солдатов <cbb110@rambler.ru>
2022-05-23 16:37:40 +0300

    Release

--- MakeReport/MainForm.Designer.cs
+++ MakeReport/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MakeReport
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -38,35 +38,38 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(237, 13);
+            this.textBox1.Location = new System.Drawing.Point(108, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.Size = new System.Drawing.Size(81, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "lsa_user";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(237, 41);
+            this.textBox2.Location = new System.Drawing.Point(108, 37);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(81, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "12345";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(141, 13);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 2;
@@ -75,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(141, 44);
+            this.label2.Location = new System.Drawing.Point(12, 40);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 3;
@@ -84,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(141, 72);
+            this.label3.Location = new System.Drawing.Point(12, 68);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 5;
@@ -92,15 +95,16 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(237, 69);
+            this.textBox3.Location = new System.Drawing.Point(108, 65);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(146, 20);
+            this.textBox3.Size = new System.Drawing.Size(81, 20);
             this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "192.168.106.13";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(141, 102);
+            this.label4.Location = new System.Drawing.Point(12, 98);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 7;
@@ -108,16 +112,17 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(237, 99);
+            this.textBox4.Location = new System.Drawing.Point(108, 95);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.Size = new System.Drawing.Size(81, 20);
             this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "lsavto_work";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(144, 134);
+            this.button1.Location = new System.Drawing.Point(15, 130);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.Size = new System.Drawing.Size(93, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "Получить отчет";
             this.button1.UseVisualStyleBackColor = true;
@@ -125,6 +130,8 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -133,26 +140,39 @@
             this.Column4,
             this.Column5,
             this.Column6});
-            this.dataGridView1.Location = new System.Drawing.Point(70, 163);
+            this.dataGridView1.Location = new System.Drawing.Point(231, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(747, 260);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(744, 553);
             this.dataGridView1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сохранить в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Наименование клиента";
             this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "VIN";
             this.Column2.Name = "Column2";
-            this.Column2.Width = 200;
+            this.Column2.Width = 125;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Дата последенего ТО";
             this.Column3.Name = "Column3";
+            this.Column3.Width = 75;
             // 
             // Column4
             // 
@@ -169,21 +189,11 @@
             this.Column6.HeaderText = "Телефон ДЛ";
             this.Column6.Name = "Column6";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(272, 134);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Сохранить в файл";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(955, 450);
+            this.ClientSize = new System.Drawing.Size(979, 577);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
@@ -195,8 +205,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Прохождение техосмотра";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -215,14 +225,14 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- MakeReport/MainForm.cs
+++ MakeReport/MainForm.cs
@@ -6,11 +6,12 @@ using System.Windows.Forms;
 
 namespace MakeReport
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, System.EventArgs e)
@@ -21,23 +22,34 @@ namespace MakeReport
         }
         void dataGridInsert()
         {
+
             try
             {
                 dataGridView1.AllowUserToAddRows = false;
-                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application { SheetsInNewWorkbook = 2 };
                 Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
                 Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
-                ExcelWorkBook = ExcelApp.Workbooks.Add();
-                ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelApp.ActiveSheet;
-                int colCount = dataGridView1.ColumnCount;
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                ExcelWorkBook = ExcelApp.Workbooks.Add(Type.Missing);
+
+                ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelApp.Worksheets.get_Item(1);
+                ExcelWorkSheet.Name = "Таблица 1";
+                int colCount = dataGridView1.Columns.Count;
+                
+                for (int i = 1; i < dataGridView1.Rows.Count; i++)
                 {
+                    
                     for (int j = 0; j < colCount; j++)
                     {
-                        ExcelApp.Cells[i + 1, j + 1] = dataGridView1.Rows[i].Cells[j].Value;
+
+                        if (i == 1)
+                           ExcelWorkSheet.Cells[i, j + 1] = dataGridView1.Columns[j].HeaderText;
+                           ExcelWorkSheet.Cells[i+1,j+1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+              
+
                     }
 
                 }
+                ExcelWorkSheet.Columns.AutoFit();
                 saveFileDialog1.FileName = "Отчет ТО.xlsx";
                 if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
                     return;
@@ -63,19 +75,19 @@ namespace MakeReport
                 {
                     sqlConnection.ConnectionString = $"Server={textBox3.Text};Database={textBox4.Text};User Id={textBox1.Text};Password={textBox2.Text};";
                     sqlConnection.Open();
-                    MessageBox.Show("Connection open");
+                    //MessageBox.Show("Connection open");
                     sqlCommand.Connection = sqlConnection;
                     sqlCommand.CommandText = "select  client.descr as 'ФИО клиента'," +
-                        " FORMAT(last_TO_date, 'dd.MM.yyyy') as 'Дата последнего ТО', " +
                         " cars.vin_id as 'VIN', " +
+                        " FORMAT(last_TO_date, 'dd.MM.yyyy') as 'Дата последнего ТО', " +
                         " client.phone_kontakt as 'Телефон клиента', " +
                         " CASE WHEN truster.descr IS NULL THEN 'Нет данных' ELSE truster.descr END as 'ФИО ДЛ'," +
                         " CASE WHEN truster.telefon IS NULL THEN 'Нет данных' ELSE truster.telefon END as 'Телефон ДЛ' " +
                         " from tcavt001 as cars " +
                         " Left JOIN tccom010 as client ON cars.client_id = client.contragent_id " +
-                        " Left JOIN tccom004 as truster on cars.client_id = truster.contragent_id " +
+                        " Left JOIN tccom004 as truster on cars.client_id = truster.contragent_id and truster_id = (select min(truster_id) from tccom004 where contragent_id = cars.client_id and truster.descr is not null) " +
                         " where last_TO_date between DATEADD(YEAR, -2,GETDATE()) and DATEADD(YEAR, -1,GETDATE()) " +
-                        " GROUP BY cars.vin_id, last_TO_date,client.descr,client.phone_kontakt ,truster.descr  , truster.telefon";
+                        " GROUP BY cars.vin_id, last_TO_date,client.descr,client.phone_kontakt ,truster.descr, truster.telefon";
                     SqlDataReader reader = sqlCommand.ExecuteReader();
                     while (reader.Read())
                     {
